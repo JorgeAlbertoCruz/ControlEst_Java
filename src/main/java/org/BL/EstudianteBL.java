2a601844e58788e4bda6bb0514ebbb3dff71c622
JorgeAlbertoCruz <jorge1guerra2018@gmail.com>
2024-07-23 11:50:43 -0600

    first commit ex

---
+++ src/main/java/org/BL/EstudianteBL.java
@@ -0,0 +1,30 @@
+package org.BL;
+
+import org.DAL.EstudianteDAL;
+import org.EN.Estudiante;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class EstudianteBL {
+    // métodos de la clase BL que devuelven la funcionalidad de los métodos de la clase DAL
+    public int guardar(Estudiante estudiante) throws SQLException {
+        return EstudianteDAL.guardar(estudiante);
+    }
+
+    public int modificar(Estudiante estudiante) throws SQLException{
+        return EstudianteDAL.modificar(estudiante);
+    }
+
+    public int eliminar(Estudiante estudiante) throws SQLException{
+        return EstudianteDAL.eliminar(estudiante);
+    }
+
+    public ArrayList<Estudiante> obtenerTodos() throws SQLException{
+        return EstudianteDAL.obtenerTodos();
+    }
+
+    public ArrayList<Estudiante> obtenerDatosFiltrados(Estudiante estudiante) throws SQLException{
+        return EstudianteDAL.obtenerDatosFiltrados(estudiante);
+    }
+}
